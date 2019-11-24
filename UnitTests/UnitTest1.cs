20385faa562352ab6abac6afb3c19003cc064cd3
Сердар Атамурадов <serdaratamuradov24@gmail.com>
2019-11-24 11:52:25 +0300

    2

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Domain.Abstract;
+using Domain.Entities;
+using System.Collections.Generic;
+using WebUI.Controllers;
+using System.Linq;
 
 namespace UnitTests
 {
@@ -7,8 +13,28 @@ namespace UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book{BookID = 1, Name = "Book1"},
+                new Book{BookID = 2, Name = "Book2"},
+                new Book{BookID = 3, Name = "Book3"},
+                new Book{BookID = 4, Name = "Book4"},
+                new Book{BookID = 5, Name = "Book5"}
+            });
+
+            BooksController controller = new BooksController(mock.Object);
+            controller.pageSize = 3;
+
+            IEnumerable<Book> result = (IEnumerable<Book>)controller.List(2).Model;
+
+            List<Book> books = result.ToList();
+            Assert.IsTrue(books.Count == 2);
+            Assert.AreEqual(books[0].Name, "Book4");
+            Assert.AreEqual(books[1].Name, "Book5");
+
         }
     }
 }
--- WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Moq;
 using Domain.Abstract;
 using Domain.Entities;
+using Domain.Concrete;
 
 namespace WebUI.Infrastructure
 {
@@ -22,14 +23,14 @@ namespace WebUI.Infrastructure
 
         private void AddBindings()
         {
-            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            /*Mock<IBookRepository> mock = new Mock<IBookRepository>();
             mock.Setup(m => m.Books).Returns(new List<Book>
             {
                 new Book { Name = "Язык программиования C# 5.0 и платформа .NET 4.5", Author = "Троелсен Э.", Price = 50},
                 new Book { Name = "C# и платформа .NET 4.5 для профессионалов", Author = "Карли Уотсон и др.", Price = 55},
                 new Book { Name = "Асинхронное программиование в C# 5.0", Author = "Дэвис А.", Price = 38},
-            });
-            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+            });*/
+            kernel.Bind<IBookRepository>().To<EFBookRepository>();
         }
 
         public object GetService(Type serviceType)
