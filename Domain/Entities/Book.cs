cc6f357d4e2ae7d3cbd49e00d9709cee763ff904
Сердар Атамурадов <serdaratamuradov24@gmail.com>
2019-11-24 10:59:14 +0300

    Upto-InsertDatas

--- Domain/Entities/Book.cs
+++ Domain/Entities/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    class Book
+    public class Book
     {
         public int BookID { get; set; }
         public string Name { get; set; }
--- WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Moq;
+using Domain.Abstract;
+using Domain.Entities;
 
 namespace WebUI.Infrastructure
 {
@@ -19,7 +22,14 @@ namespace WebUI.Infrastructure
 
         private void AddBindings()
         {
-            
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book { Name = "Язык программиования C# 5.0 и платформа .NET 4.5", Author = "Троелсен Э.", Price = 50},
+                new Book { Name = "C# и платформа .NET 4.5 для профессионалов", Author = "Карли Уотсон и др.", Price = 55},
+                new Book { Name = "Асинхронное программиование в C# 5.0", Author = "Дэвис А.", Price = 38},
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
         }
 
         public object GetService(Type serviceType)
